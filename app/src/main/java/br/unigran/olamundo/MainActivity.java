6134f7b2384779d2ff98b7412ca49e967088ac51
Andre <andre.martins@unigran.br>
2022-08-18 00:53:25 -0400

    mapeado componentes

--- app/src/main/java/br/unigran/olamundo/MainActivity.java
+++ app/src/main/java/br/unigran/olamundo/MainActivity.java
@@ -2,12 +2,26 @@ package br.unigran.olamundo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText nomeEntrada;
+    TextView textoResposta;
+    Button botaoOK;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nomeEntrada=findViewById(R.id.nomeId);
+        textoResposta=findViewById(R.id.respostaId);
+        botaoOK=findViewById(R.id.btnOKId);
+
+        textoResposta.setText("Aqui vai aparecer a resposta");
+
+
     }
 }
